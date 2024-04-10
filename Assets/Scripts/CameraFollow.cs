b0685ef0331c84a5146ba0e152242c61b295abc8
MSterpin <92162018+maurosterpin@users.noreply.github.com>
2024-04-10 17:02:24 +0200

    Initial Unity setup

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    public Transform target;
+    public float smoothing = 5f;
+
+    Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position - target.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 targetCamPos = target.position + offset;
+
+        transform.position = Vector3.Lerp(new Vector3(transform.position.x, (float)(transform.position.y + 0.3), transform.position.z), targetCamPos, smoothing * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/PressurePLate.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class PressurePLate : MonoBehaviour
+{
+    Rigidbody myRB;
+    public bool isEnabled = true;
+    public GameObject Box;
+    Collider[] boxCollisions;
+    float boxCheckRadius = 0.5f;
+    public LayerMask boxLayer;
+
+    void Update()
+    {
+        boxCollisions = Physics.OverlapSphere(myRB.position, boxCheckRadius, boxLayer);
+
+        if (boxCollisions.Length > 0) Debug.Log("Laser disabled");
+        else isEnabled = true;
+    }
+    void Start()
+    {
+        myRB = GetComponent<Rigidbody>();
+    }
+}
