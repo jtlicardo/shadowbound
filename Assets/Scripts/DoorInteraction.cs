21b43a761021ddfc385369baeb287aaadd4f0894
Josip Tomo Licardo <licardo.josip@gmail.com>
2024-05-01 20:08:38 +0200

    Add transition between lvl 2 scenes

--- Assets/Scripts/DoorInteraction.cs
+++ Assets/Scripts/DoorInteraction.cs
@@ -1,18 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class DoorInteraction : MonoBehaviour
 {
     public Text interactionText;
+    public Animator fadeAnimator; // animator component of the fade panel
+    public string nextSceneName;
+    private bool playerInRange = false;
+
+    private void Update()
+    {
+        if (playerInRange && Input.GetKeyDown(KeyCode.X))
+        {
+            StartCoroutine(EnterBuilding());
+        }
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            Debug.Log("Player entered the door trigger");
             interactionText.enabled = true;
+            playerInRange = true;
         }
     }
 
@@ -20,8 +32,24 @@ public class DoorInteraction : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
-            Debug.Log("Player exited the door trigger");
             interactionText.enabled = false;
+            playerInRange = false;
         }
+    }
+
+    private IEnumerator EnterBuilding()
+    {
+        fadeAnimator.SetTrigger("FadeOut");
+        yield return new WaitForSeconds(1);
+
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(nextSceneName);
+
+        // Wait until the scene fully loads
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+
+        SceneManager.LoadScene(nextSceneName);
     }
 }
