220aa8a63ff9d2a112d5508b024d33bdebba912f
Josip Tomo Licardo <licardo.josip@gmail.com>
2024-05-01 19:11:17 +0200

    Add door interaction script

---
+++ Assets/Scripts/DoorInteraction.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DoorInteraction : MonoBehaviour
+{
+    public Text interactionText;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Player entered the door trigger");
+            interactionText.enabled = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Player exited the door trigger");
+            interactionText.enabled = false;
+        }
+    }
+}
