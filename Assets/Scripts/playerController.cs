c11d3ad5ac593d6100bb27a91882c04d5194a4cd
alanbubalo <75133040+alanbubalo@users.noreply.github.com>
2024-05-04 21:17:08 +0200

    lvl4 fixes

--- Assets/Scripts/playerController.cs
+++ Assets/Scripts/playerController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class playerController : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
     public bool isAlive = true;
 
