e5e4650e8ec269bdf333f2be8cfecb8a72d5d8ff
MSterpin <92162018+maurosterpin@users.noreply.github.com>
2024-05-04 20:26:53 +0200

    Fixed Player getting stuck mid air in ground layer object

--- Assets/Scripts/playerController.cs
+++ Assets/Scripts/playerController.cs
@@ -17,11 +17,13 @@ public class playerController : MonoBehaviour
 
     bool grounded = false;
     Collider[] bodyGroundCollisions;
+    Collider[] bodyBoxCollisions;
     Collider[] enemyCollisions;
     Collider[] groundCollisions;
     Collider[] laserCollisions;
     Collider[] lightCollisions;
     float groundCheckRadius = 0.2f;
+    public LayerMask boxLayer;
     public LayerMask enemyLayer;
     public LayerMask groundLayer;
     public LayerMask laserLayer;
@@ -47,7 +49,8 @@ public class playerController : MonoBehaviour
     void FixedUpdate()
     {
         bodyGroundCollisions = Physics.OverlapSphere(bodyGroundCheck.position, 1.2f, groundLayer);
-        if (grounded && Input.GetAxis("Jump")>0) {
+        bodyBoxCollisions = Physics.OverlapSphere(groundCheck.position, 0.5f, boxLayer);
+        if (grounded && Input.GetAxis("Jump")>0 && bodyBoxCollisions.Length < 1) {
             grounded = false;
             myAnim.SetBool("grounded", grounded);
             myRB.AddForce(new Vector3(0, jumpHeight, 0));
@@ -58,7 +61,7 @@ public class playerController : MonoBehaviour
         lightCollisions = Physics.OverlapSphere(myRB.position, groundCheckRadius, lightLayer);
 
         groundCollisions = Physics.OverlapSphere(groundCheck.position, groundCheckRadius, groundLayer);
-        if (groundCollisions.Length > 0) grounded = true;
+        if (groundCollisions.Length > 0 || bodyBoxCollisions.Length > 0) grounded = true;
         else grounded = false;
 
         if (enemyCollisions.Length > 0 && lightCollisions.Length > 0) isAlive = false;
@@ -68,7 +71,7 @@ public class playerController : MonoBehaviour
 
         float move = Input.GetAxis("Horizontal");
         myAnim.SetFloat("speed", Mathf.Abs(move));
-        if (bodyGroundCollisions.Length < 1) myRB.velocity = new Vector3(move * runSpeed, myRB.velocity.y, 0);
+        if (bodyGroundCollisions.Length < 1 || groundCollisions.Length > 0) myRB.velocity = new Vector3(move * runSpeed, myRB.velocity.y, 0);
         
         if (move>0 && !facingRight) Flip();
         else if (move<0 && facingRight) Flip();
