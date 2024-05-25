d0e352798536dba40d3043c1a6d579c1aa08103d
MSterpin <92162018+maurosterpin@users.noreply.github.com>
2024-05-25 12:23:37 +0200

    Add platform to Level 5

---
+++ Assets/Scripts/EnemyAITest.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class EnemyAITest : MonoBehaviour
+{
+    public Transform[] waypoints; 
+    public float speed = 2.0f;
+    private int currentWaypointIndex = 0;
+
+    void Update()
+    {
+        Transform wp = waypoints[currentWaypointIndex];
+
+        if (Vector3.Distance(transform.position, wp.position) < 0.1f) {
+            if (currentWaypointIndex == 0) currentWaypointIndex = 1;
+            else currentWaypointIndex = 0;
+        }
+        else {
+            transform.position = Vector3.MoveTowards(transform.position, wp.position, speed * Time.deltaTime);
+            transform.LookAt(wp.position);
+        }
+    }
+}
