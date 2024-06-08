d2b40b761529e24a7b1726804ddf9137d633343b
Josip Tomo Licardo <licardo.josip@gmail.com>
2024-06-08 19:07:01 +0200

    Ensure the player moves with the platform

---
+++ Assets/TriggerChild.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerChild : MonoBehaviour
+{
+    private HorizontallyMovingPlatform parentScript;
+
+    void Start()
+    {
+        parentScript = GetComponentInParent<HorizontallyMovingPlatform>();
+
+        if (parentScript == null)
+        {
+            Debug.LogError("Parent script not found!");
+        }
+    }
+
+    // these functions call the parent script's functions when the player enters/exits the trigger
+    // parent script: HorizontallyMovingPlatform.cs
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            parentScript.HandleEnter(other.transform);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            parentScript.HandleExit(other.transform);
+        }
+    }
+}
