b3c7553c46f73f5ee6befbda9f0545c8e1224eb6
MSterpin <92162018+maurosterpin@users.noreply.github.com>
2024-06-08 19:09:34 +0200

    Add Level 2 Enemy AudioPause script

---
+++ Assets/Scripts/AudioControlOnPause.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class AudioControlOnPause : MonoBehaviour
+{
+    private AudioSource audioSource;
+    private bool wasPlayingBeforePause;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        if (Time.timeScale == 0)
+        {
+            if (audioSource.isPlaying)
+            {
+                audioSource.Pause();
+                wasPlayingBeforePause = true;
+            }
+        }
+        else if (Time.timeScale == 1)
+        {
+            if (wasPlayingBeforePause)
+            {
+                audioSource.UnPause();
+                wasPlayingBeforePause = false;
+            }
+        }
+    }
+}
