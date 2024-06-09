2e61b133be53ed6f6cc969cb1f37edfbffb5a6a8
Josip Tomo Licardo <licardo.josip@gmail.com>
2024-06-09 19:04:16 +0200

    Update BackgroundMusicManager.cs

--- Assets/Scripts/BackgroundMusicManager.cs
+++ Assets/Scripts/BackgroundMusicManager.cs
@@ -5,32 +5,16 @@ using UnityEngine;
 public class BackgroundMusicManager : MonoBehaviour
 {
     private AudioSource audioSource;
-    private bool wasPlayingBeforePause;
 
     void Start()
     {
         audioSource = GetComponent<AudioSource>();
+
         if (audioSource == null)
         {
             Debug.LogError("AudioSource component not found!");
         }
-        wasPlayingBeforePause = false;
-    }
 
-    void Update()
-    {
-        if (audioSource == null)
-            return;
-
-        if (Time.timeScale == 0 && audioSource.isPlaying)
-        {
-            wasPlayingBeforePause = true;
-            audioSource.Pause();
-        }
-        else if (Time.timeScale == 1 && !audioSource.isPlaying && wasPlayingBeforePause)
-        {
-            audioSource.Play();
-            wasPlayingBeforePause = false;
-        }
+        audioSource.Play();
     }
 }
