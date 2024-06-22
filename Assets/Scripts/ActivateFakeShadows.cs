db43a02cbffb99e93f1ed9f26ad83e7b71b72651
Josip Tomo Licardo <licardo.josip@gmail.com>
2024-06-22 23:21:58 +0200

    Lvl 2 WIP

---
+++ Assets/Scripts/ActivateFakeShadows.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateHugeRotatingShit : MonoBehaviour
+{
+    public GameObject[] shadowCubes;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            foreach (GameObject cube in shadowCubes)
+            {
+                cube.SetActive(true);
+            }
+        }
+    }
+}
