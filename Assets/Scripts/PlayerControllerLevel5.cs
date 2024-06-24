f6f698cd2a59669018ac5e7e03af971c5183680f
MSterpin <92162018+maurosterpin@users.noreply.github.com>
2024-06-24 17:25:52 +0200

    Revert laser collider

--- Assets/Scripts/PlayerControllerLevel5.cs
+++ Assets/Scripts/PlayerControllerLevel5.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class PlayerControllerLevel5 : MonoBehaviour
 {
     public bool isAlive = true;
 
