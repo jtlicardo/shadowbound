376626e664c67832a0e1f5680769e82819947e26
Josip Tomo Licardo <licardo.josip@gmail.com>
2024-06-29 16:19:42 +0200

    Add ObjectInteraction script

---
+++ Assets/Scripts/ObjectInteraction.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class ObjectInteraction : MonoBehaviour
+{
+    public GameObject panel;
+    private bool hasTriggered = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!hasTriggered && other.CompareTag("Player"))
+        {
+            panel.SetActive(true);
+        }
+    }
+
+    private void Update()
+    {
+        if (panel.activeSelf && Input.GetKeyDown(KeyCode.X))
+        {
+            panel.SetActive(false);
+            hasTriggered = true;
+        }
+    }
+}
