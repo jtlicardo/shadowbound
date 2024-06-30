6c9cfd0ae98abbfad2a11306a19ae2a2bc71583c
Josip Tomo Licardo <licardo.josip@gmail.com>
2024-06-30 17:05:50 +0200

    Add exit building trigger

---
+++ Assets/MoveToNextScene.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MoveToNextScene : MonoBehaviour
+{
+    public GameObject panel; // Canvas to show
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            panel.SetActive(true);
+        }
+    }
+
+    private void Update()
+    {
+        if (panel.activeSelf && Input.GetKeyDown(KeyCode.X))
+        {
+            panel.SetActive(false);
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
